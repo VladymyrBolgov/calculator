26698ec60919d4cd63eb5f8fa3d054fba4fcc199
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-04-15 14:35:02 +0300

    fin

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ function addToScreen(val) {
   function clearScreen() {
     document.getElementById("screen").value = "";
   }
-  
-//   function backspace() {
-//     var screen = document.getElementById("screen");
-//     var screenValue = screen.value;
-//     screen.value = screenValue.substring(0, screenValue.length - 1);
-// }
+ 
+  function backspace() {
+    var screen = document.getElementById("screen");
+    var screenValue = screen.value;
+    screen.value = screenValue.substring(0, screenValue.length - 1);
+}
   
 function toggleSign() {
   var screen = document.getElementById("screen");
