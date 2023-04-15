366a4fa627b047043f7794bbe5c64f31da093a02
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-04-15 13:41:34 +0300

    +/-, клавиатура

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ function addToScreen(val) {
     var screen = document.getElementById("screen");
     var screenValue = screen.value;
     screen.value = screenValue.substring(0, screenValue.length - 1);
+}
+  
+function toggleSign() {
+  var screen = document.getElementById("screen");
+  var screenValue = screen.value;
+  if (screenValue === "") {
+    return;
   }
+  var currentValue = parseFloat(screenValue);
+  var newValue = -currentValue;
+  screen.value = newValue.toString();
+}
   
   function calculate() {
     var screen = document.getElementById("screen");
@@ -22,4 +33,28 @@ function addToScreen(val) {
     
     var result = eval(screenValue);
     screen.value = result;
+}
+
+document.addEventListener('keydown', function(event) {
+  var key = event.key;
+  if (/[0-9\+\-\*\/\(\)\.\s]/.test(key)) {
+      addToScreen(key);
+  } else if (key === 'Backspace' || key === 'Delete') {
+      backspace();
+  } else if (key === 'Enter') {
+      calculate();
   }
+});
+
+
+var memoryValue = "";
+
+function addToMemory() {
+  var screen = document.getElementById("screen");
+  memoryValue = screen.value;
+}
+
+function recallMemory() {
+  var screen = document.getElementById("screen");
+  screen.value += memoryValue;
+}
